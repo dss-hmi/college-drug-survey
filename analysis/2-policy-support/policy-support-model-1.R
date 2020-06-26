d2259729750e2a93902e9bc5b0ce6d437a223f7f
Andriy V. Koval <koval.andrey@gmail.com>
2020-06-26 09:05:31 -0400

    Update policy-support-model-1.R

--- analysis/2-policy-support/policy-support-model-1.R
+++ analysis/2-policy-support/policy-support-model-1.R
@@ -427,6 +427,30 @@ dsm2 %>% make_bi_bar_graph("class_standing","sex")
 dsm2 %>% make_bi_bar_graph("sex","sex")
 dsm2 %>% make_bi_bar_graph("class_standing","class_standing")
 
+library(ggmosaic)
+# make_bi_mosaic <- function(
+
+  d <- dsm2 %>% select(sex, class_standing)
+  var1 <- "class_standing"
+  var2 <- "sex"
+
+  d1 <- d %>%
+    dplyr::rename(
+      "v1" = var1
+      ,"v2" = var2
+    ) %>%
+    mutate(
+      v1 = forcats::fct_drop(v1)
+      ,v2 = forcats::fct_drop(v2)
+    )
+
+  mosaicplot(~v1 + v2, data = d1,
+             main = paste0("Bivariate distribution between ", var1, " and ", var2)
+             ,xlab = var1, y = var2
+             ,shade = TRUE)
+
+
+
 dt1 %>%
   ggplot(aes(x = returned_to_care, y = n_people, fill = letter_sent))+
   geom_col(position = position_dodge())+
